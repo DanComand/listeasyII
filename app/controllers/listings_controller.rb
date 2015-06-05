f2b92ad868201b7326520978804d501cd599e797
Dan Comand <dcomand@gmail.com>
2015-06-05 12:42:31 -0400

    Write listing form validations.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,50 @@
 class ListingsController < ApplicationController
-  def index
+def index
+    @listings = Listing.all
   end
 
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def new
+    @listing = Listing.new
   end
 
   def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to listings_url
+    else
+      render :edit
+    end
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    if @listing.update_attributes(listing_params)
+      redirect_to listing_path(@listing)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listing_path
+  end
+
+
+
+  private
+  def listing_params
+    params.require(:listing).permit(:address, :bedrooms, :bathrooms, :square_feet, :description)
   end
 end
+
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,52 +1,8 @@
 class Listing < ActiveRecord::Base
+  has_many :pictures
+  belongs_to :user
 
-  def index
-    @listings = Listing.all
-  end
-
-  def show
-    @listing = Listing.find(params[:id])
-  end
-
-  def new
-    @listing = Listing.new
-  end
-
-  def edit
-    @listing = Listing.find(params[:id])
-  end
-
-  def create
-    @listing = Listing.new(product_params)
-
-    if @listing.save
-      redirect_to listings_url
-    else
-      render :edit
-    end
-  end
-
-  def update
-    @listing = Listing.find(params[:id])
-    if @listing.update_attributes(listing_params)
-      redirect_to listing_path(@listing)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @listing = Listing.find(params[:id])
-    @listing.destroy
-    redirect_to listing_path
-  end
-
-
-
-  private
-  def listing_params
-    params.require(:product).permit(:address, :bedrooms, :bathrooms, :square_feet, :description)
-  end
+  validates :address, :bedrooms, :bathrooms, :square_feet, :description, presence: true
 end
 
 
